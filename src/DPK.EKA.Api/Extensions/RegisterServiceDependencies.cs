6edd4317a8fc2fee25c75ea6e66318826125d313
deepaksnath <deepak.s.nath@gmail.com>
2026-05-09 21:49:48 +0530

    Client registrations moved to infra.

--- src/DPK.EKA.Api/Extensions/RegisterServiceDependencies.cs
+++ src/DPK.EKA.Api/Extensions/RegisterServiceDependencies.cs
@@ -1,7 +1,4 @@
-﻿using Azure;
-using Azure.AI.OpenAI;
-using Azure.Search.Documents;
-using DPK.EKA.Application.Interfaces;
+﻿using DPK.EKA.Application.Interfaces;
 using DPK.EKA.Application.Models;
 using DPK.EKA.Application.Services;
 using DPK.EKA.Domain.Services;
@@ -11,10 +8,7 @@ using DPK.EKA.Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.AspNetCore.RateLimiting;
-using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Options;
-using Microsoft.SemanticKernel;
 using Serilog;
 using System.Threading.RateLimiting;
 
@@ -24,6 +18,8 @@ namespace DPK.EKA.Api.Extensions
     {
         public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
         {
+            builder.Services.AddControllers();
+
             //Infrastructure Services
             builder.Services.RegisterInfrastructureServices(builder.Configuration);
 
@@ -32,8 +28,6 @@ namespace DPK.EKA.Api.Extensions
                    .Bind(builder.Configuration.GetSection("AzureAiSettings"))
                    .ValidateDataAnnotations()
                    .ValidateOnStart();
-
-            builder.Services.AddControllers();
 
             //Logging - Serilog
             var logger = new LoggerConfiguration()
@@ -47,48 +41,15 @@ namespace DPK.EKA.Api.Extensions
 
             // Add Health Checks
             builder.Services.AddHealthChecks()
-                   .AddCheck("self", () => HealthCheckResult.Healthy(), tags: new[] { "live" });
-            
+                   .AddCheck("self", () => HealthCheckResult.Healthy(), tags: new[] { "live" })
+                   .AddSqlServer(connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+                                 name: "sql-db",
+                                 tags: new[] { "ready" });
+
             // Global Exception Handling
             builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
             builder.Services.AddProblemDetails();
-                        
-            // Azure Clients
-            builder.Services.AddSingleton(sp =>
-                    {
-                        var s = sp.GetRequiredService<IOptions<AzureAiSettings>>().Value;
-                        return new AzureOpenAIClient(new Uri(s.AzureOpenAiEndpoint),
-                                                     new AzureKeyCredential(s.AzureOpenAiApiKey));
-                    });
-
-            builder.Services.AddSingleton(sp =>
-                    {
-                        var s = sp.GetRequiredService<IOptions<AzureAiSettings>>().Value;
-                        return new SearchClient(new Uri(s.SearchEndpoint),
-                                                s.SearchIndexName,
-                                                new AzureKeyCredential(s.SearchApiKey));
-                    });
-
-            // Semantic Kernal Clients
-            builder.Services.AddSingleton(sp =>
-            {
-                var s = sp.GetRequiredService<IOptions<AzureAiSettings>>().Value;
-
-                var kernelBuilder = Kernel.CreateBuilder();
-
-                kernelBuilder.AddAzureOpenAIChatCompletion(
-                    deploymentName: s.ChatDeployment,
-                    endpoint: s.AzureOpenAiEndpoint,
-                    apiKey: s.AzureOpenAiApiKey);
-
-                kernelBuilder.AddAzureOpenAITextEmbeddingGeneration(
-                    deploymentName: s.EmbeddingDeployment,
-                    endpoint: s.AzureOpenAiEndpoint,
-                    apiKey: s.AzureOpenAiApiKey);
-
-                return kernelBuilder.Build();
-            });
-
+            
             // Services
             builder.Services.AddScoped<IDocumentIngestionService, DocumentIngestionService>();
             builder.Services.AddScoped<IConversationService, ConversationService>();
