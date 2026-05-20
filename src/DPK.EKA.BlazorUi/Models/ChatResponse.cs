45eea1d024610bf7fef486e5136a08f5bec1fb4a
deepaksnath <deepak.s.nath@gmail.com>
2026-05-20 12:15:09 +0530

    Blazor Ui  chat interface added.

---
+++ src/DPK.EKA.BlazorUi/Models/ChatResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DPK.EKA.BlazorUi.Models
+{
+    public record ChatResponse
+    {
+        [JsonPropertyName("version")]
+        public string Version { get; init; } = "";
+
+        [JsonPropertyName("data")]
+        public ChatDataPayload Data { get; init; } = new();
+    }
+
+    public record ChatDataPayload
+    {
+        [JsonPropertyName("answer")]
+        public string Answer { get; init; } = "";
+
+        [JsonPropertyName("sources")]
+        public List<string> Sources { get; init; } = new();
+    }
+}
---
+++ src/DPK.EKA.BlazorUi/Services/LlmService.cs
@@ -0,0 +1,36 @@
+﻿using DPK.EKA.BlazorUi.Models;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+
+namespace DPK.EKA.BlazorUi.Services.LlmService
+{
+    public class LlmService
+    {
+        private readonly HttpClient _http;
+
+        public HttpClient Client => _http;
+
+        public LlmService(HttpClient http) => _http = http;
+
+        public async Task<ChatResponse> SendMessageAsync(ChatRequest request)
+        {
+            var response = await _http.PostAsJsonAsync("api/v1/rag/query", request);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<ChatResponse>() ?? new ChatResponse();
+        }
+
+        public async Task<bool> UploadPdfAsync(string fileName, Stream fileStream)
+        {
+            using var content = new MultipartFormDataContent();
+            using var streamContent = new StreamContent(fileStream);
+            streamContent.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+            content.Add(streamContent, "file", fileName);
+
+            // Keeping this flexible until you map your upload route
+            var response = await _http.PostAsync("api/v1/documents/upload", content);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
